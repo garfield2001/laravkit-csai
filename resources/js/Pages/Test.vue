9811f08c4f1322c8acffb87c53e2f41df485f8ab
Kamil <78793080+garfield2001@users.noreply.github.com>
2025-03-26 04:38:35 +0800

    vue + inertia
    integration of vue + inertia. working

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-amber-900 text-9xl">Test</h1>
+    </div>
+
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -9,5 +10,11 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+
 });
